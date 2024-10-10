2e54136aa6e70acc3b9881b26ae2db2563b051a3
David Tarkhnishvili <datotarkh@gmail.com>
2024-10-10 16:01:38 +0400

    added reusable nav and main components

---
+++ app/Main/Main.jsx
@@ -0,0 +1,10 @@
+
+import './Main.css'
+
+export const Main = (props) => {
+  return (
+    <div className='main-container'>
+        {props.children}
+    </div>
+  )
+}
---
+++ app/Navigation/Navbar.jsx
@@ -0,0 +1,23 @@
+
+import Link from 'next/link';
+import './Navbar.css'
+
+const Navbar = () => {
+
+
+
+  return (
+    <div className='nav-container'>
+       <ul>
+        <Link className='nav-link' href='/'> <li>Home</li></Link>
+        <Link className='nav-link' href='/about'> <li >About</li></Link>
+        <Link className='nav-link' href='/blog'> <li >Blog</li></Link>
+        <Link className='nav-link' href='/products'> <li >Products</li></Link>
+        
+       </ul>
+
+    </div>
+  )
+}
+
+export default Navbar
