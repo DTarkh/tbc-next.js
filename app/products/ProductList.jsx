750860c4ac59d5b364604afb6cc79effa3615b2b
David Tarkhnishvili <datotarkh@gmail.com>
2024-10-10 18:46:36 +0400

    added product lists dinamically

---
+++ app/products/ProductList.jsx
@@ -0,0 +1,21 @@
+import "./ProductList.css";
+
+const ProductCard = ({ product }) => {
+  return (
+    <div className="ProductCard" key={product.id}>
+      <h2 className="ProductCard-title">{product.title}</h2>
+      <p className="ProductCard-description">{product.description}</p>
+      <p className="ProductCard-category">Category: {product.category}</p>
+      <p className="ProductCard-price">Price: ${product.price}</p>
+      <p className="ProductCard-discount">Discount: {product.discountPercentage}%</p>
+      <p className="ProductCard-rating">Rating: {product.rating}/5</p>
+      <p className="ProductCard-stock">Stock: {product.stock} units</p>
+      <p className="ProductCard-brand">Brand: {product.brand}</p>
+      
+      <p className="ProductCard-warranty">Warranty: {product.warrantyInformation}</p>
+      <p className="ProductCard-shipping">Shipping: {product.shippingInformation}</p>
+    </div>
+  );
+};
+
+export default ProductCard;
--- app/products/page.jsx
+++ app/products/page.jsx
@@ -1,19 +1,20 @@
-import React from 'react'
-import Main from '../Wrap/Main'
+import React from "react";
+import ProductCard from "./ProductList";
 
 const ProductsPage = async () => {
-
-    const res = await fetch("https://dummyjson.com/products#products-all")
-    const { products } = await res.json()
+  const res = await fetch("https://dummyjson.com/products#products-all");
+  const { products } = await res.json();
 
   return (
-    <div>
-        <Main>
-            <h1>Products</h1>
-            {products.map((product) =>(<p>{product.title}</p>))}
-        </Main>
+    <div className="products-container">
+    
+      {products.map((product) => (
+        <ProductCard  product={product} />
+      ))}
+      
+    
     </div>
-  )
-}
+  );
+};
 
 export default ProductsPage;
