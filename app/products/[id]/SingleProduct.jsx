0423dc377effbbea166c8b421a9c953ec2e4b293
David Tarkhnishvili <datotarkh@gmail.com>
2024-10-11 09:54:42 +0400

    added more styles to product single page

--- app/products/[id]/SingleProduct.jsx
+++ app/products/[id]/SingleProduct.jsx
@@ -3,28 +3,30 @@ import "./SingleProduct.css";
 const SingleProduct = ({ product }) => {
   return (
     <div className="singleProduct-container">
-      <div className="ProductCard" key={product.id}>
-        <div className="card-image">
-          <img src={product.thumbnail} alt={"image"} />
-        </div>
-        <h2 className="ProductCard-title">{product.title}</h2>
-        <p className="ProductCard-description">{product.description}</p>
-        <p className="ProductCard-category">Category: {product.category}</p>
-        <p className="ProductCard-price">Price: ${product.price}</p>
-        <p className="ProductCard-discount">
-          Discount: {product.discountPercentage}%
-        </p>
-        <p className="ProductCard-rating">Rating: {product.rating}/5</p>
-        <p className="ProductCard-stock">Stock: {product.stock} units</p>
-        <p className="ProductCard-brand">Brand: {product.brand}</p>
+      <div className="product-details-container" key={product.id}>
+  <div className="product-image">
+    <img src={product.thumbnail} alt="Product image" />
+  </div>
 
-        <p className="ProductCard-warranty">
-          Warranty: {product.warrantyInformation}
-        </p>
-        <p className="ProductCard-shipping">
-          Shipping: {product.shippingInformation}
-        </p>
-      </div>
+  <div className="product-info">
+    <h2 className="product-title">{product.title}</h2>
+    <p className="product-description">{product.description}</p>
+
+    <div className="product-meta">
+      <p className="product-category">Category: {product.category}</p>
+      <p className="product-price">Price: ${product.price}</p>
+      <p className="product-discount">Discount: {product.discountPercentage}%</p>
+      <p className="product-rating">Rating: {product.rating}/5</p>
+      <p className="product-stock">Stock: {product.stock} units</p>
+      <p className="product-brand">Brand: {product.brand}</p>
+    </div>
+
+    <div className="product-additional-info">
+      <p className="product-warranty">Warranty: {product.warrantyInformation}</p>
+      <p className="product-shipping">Shipping: {product.shippingInformation}</p>
+    </div>
+  </div>
+</div>
     </div>
   );
 };
