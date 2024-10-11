9f66293d9d85121757caef71b8bdafd7c06de313
David Tarkhnishvili <datotarkh@gmail.com>
2024-10-11 11:02:26 +0400

    added blog single page dinamically and styled it

--- app/blog/[id]/page.jsx
+++ app/blog/[id]/page.jsx
@@ -2,11 +2,36 @@ import "./singleBlog.css";
 
 const Page = async ({ params: { id } }) => {
   const res = await fetch(`https://dummyjson.com/posts/${id}`);
-  const post = await res.json()
+  const post = await res.json();
 
   return (
-    <div className="singleBlog-container">
-      <h1 key={post.id}>{post.title}</h1>
+    <div className="blog-post-container">
+      <div className="blog-post-card">
+
+      <h2 className="blog-post-title">{post.title}</h2>
+      <p className="blog-post-body">{post.body}</p>
+
+      <div className="blog-post-meta">
+        <div className="blog-post-reactions">
+          <span className="reaction reaction-likes">
+            👍 {post.reactions.likes}
+          </span>
+          <span className="reaction reaction-dislikes">
+            👎 {post.reactions.dislikes}
+          </span>
+        </div>
+
+        <div className="blog-post-views">
+          <span>👁️ {post.views} views</span>
+        </div>
+        <div className="blog-tags">
+          {post.tags.map((tag, index) => <span  key={index}className="tag">{tag}</span>)}
+  
+  
+</div>
+      </div>
+
+      </div>
     </div>
   );
 };
