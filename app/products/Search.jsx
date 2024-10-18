009d4c9175b88debe5bbdeca295f65c1c23fd5c3
David Tarkhnishvili <datotarkh@gmail.com>
2024-10-18 09:22:55 +0400

    added debouncer to the search component

--- app/products/Search.jsx
+++ app/products/Search.jsx
@@ -2,18 +2,21 @@
 
 import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
+import useDebounce from "./hooks/useDebounce"
 
 const Search = () => {
   const router = useRouter();
   const [value, setValue] = useState("");
+  const debounce = useDebounce(value, 1000);
+
 
   const handleChange = (event) => {
     setValue(event.target.value);
   };
 
   useEffect(() => {
-    router.push(`/products?search=${value}`);
-  }, [value, router]);
+    router.push(`/products?search=${debounce}`);
+  }, [debounce, router]);
 
   return (
     <div>
