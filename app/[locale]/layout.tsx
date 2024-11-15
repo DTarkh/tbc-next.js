241ace779134350e62b8d1029cbe753626e4a05f
David Tarkhnishvili <datotarkh@gmail.com>
2024-11-15 19:50:22 +0100

    fix locale await issue

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -10,11 +10,13 @@ const roboto = Roboto_Serif({ subsets: ['latin']})
  
 export default async function LocaleLayout({
   children,
-  params: {locale}
+  params
 }: {
   children: React.ReactNode;
   params: {locale: string};
 }) {
+
+  const { locale } = await params
   // Ensure that the incoming `locale` is valid
   if (!routing.locales.includes(locale as any)) {
     notFound();
