f45bd9d0458d0eec4962dbc4a90de5cb0e0d37c7
David Tarkhnishvili <datotarkh@gmail.com>
2024-11-17 17:39:17 +0100

    add sort

--- app/[locale]/(dashboard)/products/page.jsx
+++ app/[locale]/(dashboard)/products/page.jsx
@@ -6,11 +6,11 @@ import fetchProducts from "../../Components/hooks/useProducts";
 
 
 const ProductsPage = async ({searchParams}) => {
-  const { search } = await searchParams;
-  console.log('onpage' , search);
+  const { search, sortBy, order } = await searchParams;
+  console.log('onpage' , sortBy, order);
   
 
-  const products = await fetchProducts(search);
+  const products = await fetchProducts(search, sortBy, order);
   // const [currentProduct, setCurrentProduct] = useState([]);
   // const [active, setActive] = useState(false);
   // const [isActive, setIsActive] = useState(false);
@@ -41,7 +41,7 @@ const ProductsPage = async ({searchParams}) => {
 
       <div className="grid grid-cols-1 md:grid-cols-3 p-5">
         <aside className="col-span-1 md:col-span-1">
-          <NavPanel onAdd={onAdd} />
+          <NavPanel sortBy={sortBy} order={order}/>
         </aside>
 
         <main className="col-span-1 md:col-span-2">
--- app/[locale]/Components/NavPanel.jsx
+++ app/[locale]/Components/NavPanel.jsx
@@ -1,8 +1,20 @@
-
+'use client'
 import Search from "../Components/Search";
-import Serch from "../Components/Search"
+import { useRouter } from "next/navigation";
 
-const NavPanel = ({ onAdd }) => {
+
+const NavPanel = () => {
+
+  const router = useRouter()
+
+const handleChange = (e) =>{
+  const target = e.target.value
+  const [sortBy, order] = target.split('-')
+  router.push(`products?sortBy=${sortBy}&order=${order}`)
+  
+}
+
+
   return (
     <div className="w-full bg-gray-100 p-20 rounded-lg shadow-md">
       <h2 className="text-lg font-semibold text-gray-700 mb-4">
@@ -24,21 +36,21 @@ const NavPanel = ({ onAdd }) => {
 
       <div className="mb-4">
         <h3 className="text-sm font-semibold text-gray-600 mb-2">Sort By</h3>
-        <select className="w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500">
+        <select onChange={handleChange} className="w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500">
           <option disabled selected>
             Price
           </option>
           <option value="price-asc">Price: Low to High</option>
           <option value="price-desc">Price: High to Low</option>
         </select>
-        <select className="w-full p-2 mt-2 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500">
+        <select  onChange={handleChange} className="w-full p-2 mt-2 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500">
           <option disabled selected>
             Discount
           </option>
           <option value="discount-desc">Discount: High to Low</option>
           <option value="discount-asc">Discount: Low to High</option>
         </select>
-        <select className="w-full p-2 mt-2 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500">
+        <select onChange={handleChange} className="w-full p-2 mt-2 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500">
           <option disabled selected>
             Rating
           </option>
