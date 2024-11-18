24590d141e5ba34a8351d965518d4938c9fe0b37
Guranda Lemonjava <109870250+guranda26@users.noreply.github.com>
2024-11-19 03:19:24 +0400

    add generic type for the usedebounce value and number type for delay

--- app/[locale]/Components/hooks/useDebounce.tsx
+++ app/[locale]/Components/hooks/useDebounce.tsx
@@ -2,8 +2,8 @@
 
 import { useEffect, useState } from "react";
 
-const useDebounce = (value, delay) => {
-  const [debouncedValue, setDebouncedValue] = useState(value);
+const useDebounce = <T,>(value: T, delay: number): T => {
+  const [debouncedValue, setDebouncedValue] = useState<T>(value);
 
   useEffect(() => {
     const timer = setTimeout(() => {
