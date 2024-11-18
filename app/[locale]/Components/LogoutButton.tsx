c792b5e25323aff645b85d7bf4f3b303713a3938
Guranda Lemonjava <109870250+guranda26@users.noreply.github.com>
2024-11-19 02:29:13 +0400

    switch img tag with Image

---
+++ app/[locale]/Components/LogoutButton.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import { RiLogoutBoxLine } from "react-icons/ri";
+
+const LogoutButton = () => {
+  const router = useRouter();
+
+  const handleLogout = () => {
+    localStorage.clear();
+
+    router.push("/login");
+  };
+
+  return (
+    <div
+      onClick={handleLogout}
+      className="flex items-center text-gray-700 hover:text-orange-500 cursor-pointer"
+    >
+      <p>Logout:</p>
+      <RiLogoutBoxLine />
+    </div>
+  );
+};
+
+export default LogoutButton;
